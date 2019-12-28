c3800aafbec4540915ad1a0cc02770df8c45c064
glugg23 <m.leonhardt424@gmail.com>
2019-12-28 18:13:55 +0000

    Rewrite event routes to be async

--- routes/events/events.ts
+++ routes/events/events.ts
@@ -9,41 +9,30 @@ const db = new Database();
  * GET mapping for base URL of Events page
  * Fetches list of events from database
  */
-router.get('/', (req: Request, res: Response) => {
-    db.getCurrentYearTerm().then(current => {
-        db.getEventsFor(current.year_id, current.term_id).then(events => {
-            let displayableEvents = events.map(event => event.prettifyDates());
-    
-            res.render('events', {
-                title: 'Events',
-                events: displayableEvents
-            });
-        });
+router.get('/', async (req: Request, res: Response) => {
+    let current = await db.getCurrentYearTerm();
+    let events = await db.getEventsFor(current.year_id, current.term_id);
+
+    res.render('events', {
+        events: events.map(event => event.prettifyDates())
     });
 });
 
-router.get('/:year(\\d{2}-\\d{2})/:term(tr[1-3])', (req: Request, res: Response) => {
-    let yearPromise = db.getYear(req.params.year);
-    let termPromise = db.getTerm(req.params.term);
+router.get('/:year(\\d{2}-\\d{2})/:term(tr[1-3])', async (req: Request, res: Response) => {
+    let year = await db.getYear(req.params.year);
+    let term = await db.getTerm(req.params.term);
 
-    Promise.all([yearPromise, termPromise]).then(values => {
-        let year = values[0];
-        let term = values[1];
+    //Term cannot be incorrect so only validate year
+    if(year === undefined) {
+        res.redirect('/events');
 
-        //Term cannot be incorrect so only validate year
-        if(year === undefined) {
-            res.redirect('/events');
+    } else {
+        let events = await db.getEventsFor(year.id, term.id);
 
-        } else {
-            db.getEventsFor(year.id, term.id).then(events => {
-                let displayableEvents = events.map(event => event.prettifyDates());
-
-                res.render('events', {
-                    events: displayableEvents
-                });
-            });
-        }
-    });
+        res.render('events', {
+            events: events.map(event => event.prettifyDates())
+        });
+    }
 });
 
 export default { router };
