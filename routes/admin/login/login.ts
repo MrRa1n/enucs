b74014b94bad748bc6a89dc620043558b274ceab
Toby Cook <tobycook@enucs.org.uk>
2021-05-30 22:42:38 +0100

    Added login code body

--- routes/admin/login/login.ts
+++ routes/admin/login/login.ts
@@ -1,6 +1,8 @@
-import Database from "../../../database/database";
-import e, { Router, Request, Response } from "express";
 import bcrypt from 'bcrypt';
+import cookie from 'cookie';
+import crypto from 'crypto-random-string';
+import { Request, Response, Router } from "express";
+import Database from "../../../database/database";
 
 // TODO: Add logger
 
@@ -14,21 +16,41 @@ router.get('/', (req: Request, res: Response) => {
 });
 
 router.post('/', (req: Request, res: Response) => {
-    // TODO: Add input user to fetch
-    db.getUser('').then(user => {
-        // TODO: Add input password to compare
-        bcrypt.compare('', user.password, (err, same) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    console.log({ username, password });
+
+    db.getUser(username).then(user => {
+        bcrypt.compare(password, user.password, (err, same) => {
             if (err) {
                 throw err;
             } else if (!same) {
-                throw new Error('Passwords do not match!');
+                throw new Error('Password is incorrect!');
             } else {
-                // TODO: Set SESSIONID in cookie, update in database
-                // TODO: Redirect to dashboard
+                req.app.locals.userid = user.userid;
+                req.app.locals.user = username;
+                req.app.locals.authorised = true;
+
+                const sessionId = crypto(32);
+
+                res.setHeader('Set-Cookie', cookie.serialize('SESSIONID', sessionId, {
+                    maxAge: 3600,
+                    httpOnly: true,
+                    path: '/',
+                    sameSite: 'strict'
+                }));
+
+                db.setSessionId(sessionId, user.userid)
+                    .then(() => res.redirect('/admin/dashboard'))
+                    .catch(err => {
+                        console.error(err);
+                        res.status(500).send(err)
+                    });
             }
         });
     }, err => {
-        
+        console.error(err);
     })
 });
 
