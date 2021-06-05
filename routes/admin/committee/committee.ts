1b7738322fcf7e340dade16131188f1fe7eab538
Toby Cook <tobycook@enucs.org.uk>
2021-06-05 16:59:05 +0100

    implemented fetch for committee members

--- routes/admin/committee/committee.ts
+++ routes/admin/committee/committee.ts
@@ -1,7 +1,12 @@
 import { Request, Response, Router } from 'express';
+import CommitteeMember from '../../../models/committee-member';
+import Database from "../../../database/database";
+import { logger } from '../../../utils/logger';
+
 const isAuthenticated = require('../../../auth/user-auth');
 
 const router = Router();
+const db = new Database();
 
 /**
  * TODO:
@@ -14,12 +19,33 @@ const router = Router();
  * committee roles.
  */
 
+/**
+ * Date formatter
+ * Credit - https://stackoverflow.com/questions/3552461/how-to-format-a-javascript-date
+ */
+function join(t: Date, a: any[], s: string) {
+    function format(m: any) {
+        let f = new Intl.DateTimeFormat('en', m);
+        return f.format(t);
+    }
+    return a.map(format).join(s);
+}
+ 
 router.get('/admin/committee', isAuthenticated, (req: Request, res: Response) => {
+    logger.info(`[${req.method}] - ${req.path} - Fetching committee members`);
+    const dateFormat = [{day: 'numeric'}, {month: 'short'}, {year: 'numeric'}];
 
-    // TODO: Fetch committee members from database
-
-    res.render('committee', {
-        title: 'Committee'
+    db.getCommittee().then((committee: CommitteeMember[]) => {
+        committee.map(member =>
+            member.member_since = join(new Date(member.member_since), dateFormat, ' '));
+        res.render('committee', {
+            title: 'Committee',
+            committee: committee
+        });
+    })
+    .catch(err => {
+        logger.error('Failed to retrieve committee members', err);
+        res.status(500).send(err);
     });
 });
 
